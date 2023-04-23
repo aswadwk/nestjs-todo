4869611a201c56c40a965e31dcefaa6b49dfe5f5
aswadwk <hajaraswadkom@gmail.com>
2023-04-23 20:31:09 +0700

    feat: dockerfile

---
+++ build_image.sh
@@ -0,0 +1,1 @@
+docker build -t nest-todo --no-cache --build-arg MYSQL_HOST=172.18.0.1 --build-arg MYSQL_USER=xxxx --build-arg MYSQL_PASSWORD=xxx --build-arg MYSQL_DBNAME=todo4 . | tee -a log.txt
--- src/db/data.source.ts
+++ src/db/data.source.ts
@@ -2,11 +2,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.MYSQL_HOST || 'localhost',
   port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'nest_todo_db',
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DBNAME || 'nest_todo_db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 };
